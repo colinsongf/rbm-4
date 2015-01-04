62dbd8e33c65b8a949edbc35cde765f2fd91420f
Oleksiy Pylypenko <opylypenko@dataxu.com>
2015-01-04 10:47:33 +0100

    RenameFFT changes

--- src/test/java/mfcc/RenameFFTSpec.java
+++ src/test/java/mfcc/RenameFFTSpec.java
@@ -1,5 +1,11 @@
 package mfcc;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.runner.RunWith;
 
 import com.insightfullogic.lambdabehave.JunitSuiteRunner;
@@ -8,9 +14,54 @@ import static com.insightfullogic.lambdabehave.Suite.describe;
 
 @RunWith(JunitSuiteRunner.class)
 public class RenameFFTSpec {{
+
     describe("RenameFFT", it -> {
         it.should("rename uniformly", expect -> {
-            Rename
+            RenameFFT renameFFT = new RenameFFT();
+            File file = new File("A33/a33-1562010_0057.fft");
+            File newF = renameFFT.newFileName(file);
+
+            expect.that(newF).isEqualTo(new File("a33/00/a33-0001562010_0057.fft"));
         });
+
+        it.should("look everywhere", expect -> {
+            Files.walk(Paths.get("/home/oleksiyp/deeplearn/fft2")).forEach(path -> {
+                if (!Files.isRegularFile(path)) {
+                    return;
+                }
+                RenameFFT renameFFT = new RenameFFT();
+                renameFFT.newFileName(path.toFile());
+            });
+        });
+
+        it.should("look everywhere and has len 30", expect -> {
+            Files.walk(Paths.get("/home/oleksiyp/deeplearn/fft2")).forEach(path -> {
+                if (!Files.isRegularFile(path)) {
+                    return;
+                }
+                RenameFFT renameFFT = new RenameFFT();
+                File val = renameFFT.newFileName(path.toFile());
+                if (val.toString().length() != 30) {
+                    expect.failure("no len 30: " + val + ", len=" + val.toString().length());
+                }
+            });
+        });
+
+        it.should("has no clashes", expect -> {
+            Set<String> set = new HashSet<>();
+            Files.walk(Paths.get("/home/oleksiyp/deeplearn/fft2")).forEach(path -> {
+                if (!Files.isRegularFile(path)) {
+                    return;
+                }
+                RenameFFT renameFFT = new RenameFFT();
+                File val = renameFFT.newFileName(path.toFile());
+
+                if (!set.add(val.toString())) {
+                    expect.failure(val.toString());
+                }
+            });
+        });
+
     });
-}}
+}
+}
